b997fe4bac62fb8ff2a8425a5f5c2acc7a48d817
Silja <silja.vj@gmail.com>
2022-03-09 18:46:41 +0100

    done did some shit

--- CV/js.js
+++ CV/js.js
@@ -1,18 +1,20 @@
+window.onscroll = () => {
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let scrollPercent = scrollTop / (docHeight - winHeight);
+    let scrollPercentRounded = Math.round(scrollPercent * 100);
 
-    
-    window.onscroll = () => {
-        let scrollTop = window.scrollY;
-        let docHeight = document.body.offsetHeight;
-        let winHeight = window.innerHeight;
-        let scrollPercent = scrollTop / (docHeight - winHeight);
-        let scrollPercentRounded = Math.round(scrollPercent * 100);
-        
-        document.querySelector(
-         ".scroller"
-        ).style.background = `linear-gradient(to bottom, #ffff ${scrollPercentRounded}%, #0000 ${scrollPercentRounded}%)`;
-    }
+    document.querySelector(
+        ".scroller"
+    ).style.background = `linear-gradient(to bottom, #ffff ${scrollPercentRounded}%, #0000 ${scrollPercentRounded}%)`;
+}
 
-    function myHeyo() {
-        document.getElementById("Button2").innerHTML = "Heyo";
-    }
+function myHeyo() {
+    document.getElementById("Button2").innerHTML = "Heyo";
+}
 
+function getElmt(i) {
+    var elmt = document.getElementById(i);
+    elmt.scrollIntoView();
+}
