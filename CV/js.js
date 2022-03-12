dfe6c8237cbc8e44c642968e8c3f433211c227f9
Silja <silja.vj@gmail.com>
2022-03-12 15:13:22 +0100

    .

--- CV/js.js
+++ CV/js.js
@@ -20,88 +20,13 @@ function getElmt(i) {
     elmt.scrollIntoView();
 }
 
-/*const winSize = (e) => {
-    let rect = e.getBoundingClientRect();
-
-    let left = rect.left + window.scrollX;
-    let top = rect.top + window.scrollY;
-    let right = rect.right + window.scrollX;
-    let bottom = rect.bottom + window.scrollY;
-
-    if (rect.x === undefined) x = left;
-    else x = rect.x + window.scrollX;
-
-    if (rect.y === undefined) y = top;
-    else y = rect.y + window.scrollY;
-
-    let width = rect.width;
-    let height = rect.height;
-
-    return { left, top, right, bottom, width, height };
-};*/
-
-
-
 
 let cursor = document.querySelector('#cursor');
 const onMouseMove = (e) => {
     cursor.style.left = e.pageX + 'px';
-    cursor.style.rop = e.pageY + 'px';
+    cursor.style.top = e.pageY + 'px';
 }
 document.addEventListener('mousemove', onMouseMove);
-
-/*cursor.addEventListener('mousemove', e => {
-
-
-    let rect = e.target.getBoundingClientRect();
-
-    let left = rect.left + window.scrollX;
-    let top = rect.top + window.scrollY;
-    let right = rect.right + window.scrollX;
-    let bottom = rect.bottom + window.scrollY;
-
-    let x = rect.x + window.scrollX;
-
-    let y = rect.y + window.scrollY;
-
-    let width = rect.width;
-    let height = rect.height;
-
-
-
-
-    x = e.clientX - rect.left;
-    y = e.clientY - rect.top;
-    cursor.style.setProperty('--x', x + 'px');
-    cursor.style.setProperty('--y', y + 'px');
-});*/
-
-
-
-
-
-
-
-
-
-/*let winWidth = window.innerWidth;
-let winHeight = window.innerHeight;
-let x = e.clientX - winWidth.left;
-let y = e.clientY - winHeight.top;
-cursor.style.setProperty('--x', x + 'px')
-cursor.style.setProperty('--y', y + 'px')
-
-
-/*window.onmouseover = () => {
-
-    document.querySelector(
-        ".startText"
-    ).style.opacity = '10%';
-
-    document.querySelector(
-
-    )
-}
 
 /*function changeWidth(i) { SO SMART BUT NO 
 
