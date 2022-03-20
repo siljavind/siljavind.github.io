4f74b6cd1ba906b60887c7495746e078d26cfd7e
Silja <silja.vj@gmail.com>
2022-03-20 13:08:56 +0100

    .

--- script.js
+++ script.js
@@ -1,3 +1,108 @@
+// SCROLLER *******************************************************************************************************************SCROLLER**
+window.onscroll = () => {
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let scrollPercent = scrollTop / (docHeight - winHeight);
+    let scrollPercentRounded = Math.round(scrollPercent * 100);
+
+    document.querySelector(
+        ".scroller"
+    ).style.background = `linear-gradient(to bottom, #000 ${scrollPercentRounded}%, #0000 ${scrollPercentRounded}%)`;
+
+};
+
+
+// TINY FUNCTIONS *******************************************************************************************************TINY FUNCTIONS**
+function watHeyo(i) {
+    document.getElementById(i).innerHTML = "Heyo";
+};
+
+function getElmt(i) {
+    document.getElementById(i).scrollIntoView();
+};
+
+
+// LIGHT SWITCH ***********************************************************************************************************LIGHT SWITCH**
+function lightSwitch(i) {
+    let lightSwitch = document.getElementById(i);
+    let lightStyle = lightSwitch.currentStyle || window.getComputedStyle(lightSwitch);
+
+    if (lightStyle.marginLeft == '10px') {
+        lightSwitch.style.cssText = `
+        margin-left: 40px;
+        `;
+        document.body.style.cssText = `
+        background-color: rgb(252, 240, 225);
+        `;
+
+    } else {
+        lightSwitch.style.cssText = `
+        margin-left: 10px;
+        `;
+        document.body.style.cssText = `
+        background-color: black;
+        `;
+    };
+};
+
+
+// CURSOR SHADOW *********************************************************************************************************CURSOR SHADOW**
+const shadowID = ["shadow1", "shadow2", "shadow3", "shadow4"]
+
+for (let i = 0; i < shadowID.length; i++) {
+    d3.select("#shadowContainer").insert("div").classed("shadow", true).attr("id", shadowID[i]);
+};
+
+const shadowList = document.querySelectorAll(".shadow");
+
+for (let i = 0; i < shadowList.length; i++) {
+
+    document.addEventListener('mousemove', (e) => {
+        shadowList[i].style.cssText = `
+        left: ${e.clientX}px;
+        top: ${e.clientY}px;
+        `;
+    });
+
+    document.addEventListener('mousedown', (e) => {
+        shadowList[i].style.cssText = `
+        width: ${shadowList[i].clientWidth = Math.max(1500)}px;
+        height: ${shadowList[i].clientHeight = Math.max(1500)}px;
+        left: ${e.clientX}px;
+        top: ${e.clientY}px;
+        `;
+    });
+
+    document.addEventListener('mouseup', (e) => { //Why does this work?? == vs. === &= false vs. value
+        shadowList[i].style.cssText = `
+        width: ${shadowList[i].clientWidth = false}; 
+        height: ${shadowList[i].clientHeight = false};
+        left: ${e.clientX}px;
+        top: ${e.clientY}px;
+        `;
+    });
+
+    document.addEventListener('mouseleave', (e) => {
+        shadowList[i].style.cssText = `
+        width: ${shadowList[i].clientWidth = 0};
+        height: ${shadowList[i].clientHeight = 0};
+        left: ${e.clientX}px;
+        top: ${e.clientY}px;
+        `;
+    });
+
+    document.onload = onLoad();
+
+    function onLoad() {
+        shadowList[i].style.cssText = `
+        width: ${shadowList[i].clientWidth = 0};
+        height: ${shadowList[i].clientHeight = 0};
+        `;
+    };
+};
+
+// SVG BLOB *******************************************************************************************************************SVG BLOB**
 {
     const blobList = [
         "M253.3 -158.8C307.4 -52.5 316.1 67.5 266.3 147.7C216.5 228 108.3 268.5 29.8 251.3C-48.7 234.1 -97.4 159.3 -124.7 92C-152 24.8 -157.8 -34.9 -133.5 -123.9C-109.1 -213 -54.6 -331.5 22.5 -344.5C99.6 -357.5 199.2 -265 253.3 -158.8",
@@ -7,8 +112,11 @@
         "M170.4 -145.3C194.8 -56.2 170.5 14.1 134 65.3C97.4 116.6 48.7 148.8 -23.1 162.1C-95 175.5 -189.9 170 -235.7 113.4C-281.5 56.8 -278 -50.8 -230.5 -153.2C-183 -255.7 -91.5 -352.8 -9.2 -347.5C73 -342.2 146.1 -234.3 170.4 -145.3",
         "M317.3 -181C382.2 -70.7 386 77.2 322.9 141.7C259.8 206.3 129.9 187.5 24 173.7C-82 159.8 -164 150.9 -198.6 102.8C-233.2 54.7 -220.5 -32.7 -179.6 -129.2C-138.6 -225.7 -69.3 -331.3 28.4 -347.7C126.2 -364.2 252.3 -291.3 317.3 -181"
     ];
-
-    const colors = ["rgb(128, 0, 150)", "rgba(255, 100, 160, 0.9)", "rgba(255, 50, 0, 0.9)"];
+    const colors = [
+        "rgb(128, 0, 150)",
+        "rgba(255, 100, 160, 0.9)",
+        "rgba(255, 50, 0, 0.9)"
+    ];
 
     let svg = d3.select("svg"),
         currentBlobs = randomBlobs(colors.length);
@@ -21,6 +129,7 @@
         .style("fill", function(_, i) {
             return colors[i];
         })
+
     animate();
 
     function randomBlobs(numOfBlobs) {
@@ -51,110 +160,6 @@
             })
             .on("end", animate);
     };
-};
-
-
-
-window.onscroll = () => {
-    let scrollTop = window.scrollY;
-    let docHeight = document.body.offsetHeight;
-    let winHeight = window.innerHeight;
-    let scrollPercent = scrollTop / (docHeight - winHeight);
-    let scrollPercentRounded = Math.round(scrollPercent * 100);
-
-    document.querySelector(
-        ".scroller"
-    ).style.background = `linear-gradient(to bottom, #000 ${scrollPercentRounded}%, #0000 ${scrollPercentRounded}%)`;
-
-};
-
-function watHeyo(i) {
-    document.getElementById(i).innerHTML = "Heyo";
-};
-
-function getElmt(i) {
-    document.getElementById(i).scrollIntoView();
-};
-
-function lightSwitch(i) {
-    let lightSwitch = document.getElementById(i);
-    let lightStyle = lightSwitch.currentStyle || window.getComputedStyle(lightSwitch);
-
-
-    if (lightStyle.marginLeft == '10px') {
-        lightSwitch.style.cssText = `
-        margin-left: 40px;
-        `;
-        document.body.style.cssText = `
-        background-color: rgb(252, 240, 225);
-        `;
-
-    } else {
-        lightSwitch.style.cssText = `
-        margin-left: 10px;
-        `;
-        document.body.style.cssText = `
-        background-color: black;
-        `;
-    };
-};
-
-
-
-const shadowID = ["shadow1", "shadow2", "shadow3", "shadow4"]
-for (let i = 0; i < shadowID.length; i++) {
-    d3.select("#shadowContainer").insert("div").classed("shadow", true).attr("id", shadowID[i]);
-}
-
-const shadowList = document.querySelectorAll(".shadow");
-for (let i = 0; i < shadowList.length; i++) {
-
-    document.addEventListener('mousemove', (e) => {
-        shadowList[i].style.cssText = `
-        left: ${e.clientX}px;
-        top: ${e.clientY}px;
-        `;
-    });
-};
-
-
-for (let i = 0; i < shadowList.length; i++) {
-
-    document.addEventListener('mousedown', (e) => {
-        shadowList[i].style.cssText = `
-        width: ${shadowList[i].clientWidth = Math.max(1500)}px;
-        height: ${shadowList[i].clientHeight = Math.max(1500)}px;
-        left: ${e.clientX}px;
-        top: ${e.clientY}px;
-        `;
-    });
-
-    document.addEventListener('mouseup', (e) => { //Why does this work?? Or.. Not. == / ===
-        shadowList[i].style.cssText = `
-        width: ${shadowList[i].clientWidth == 1000}px; 
-        height: ${shadowList[i].clientHeight == 1000}px;
-        left: ${e.clientX}px;
-        top: ${e.clientY}px;
-        `;
-    });
-
-    document.addEventListener('mouseleave', (e) => {
-        shadowList[i].style.cssText = `
-        width: ${shadowList[i].clientWidth = 1}px;
-        height: ${shadowList[i].clientHeight = 1}px;
-        left: ${e.clientX}px;
-        top: ${e.clientY}px;
-        `;
-    })
-
-    document.addEventListener('load', onLoad());
-
-    function onLoad() {
-        shadowList[i].style.cssText = `
-        width: ${shadowList[i].clientWidth = 1}px;
-        height: ${shadowList[i].clientHeight = 1}px;
-        `;
-    }
 };
 
 //DETECTS WHEN MOUSE HASN'T BEEN MOVED FOR A DEFINED PERIOD OF TIME (10 seconds) (Needed the opposite of "mousemove")
